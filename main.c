828c589c67860c631a4f7b0d1dabd17a5ee16756
Beatriz <bbazagli@student.42sp.org.br>
2024-03-04 14:36:26 -0300

    f3: add_history() added

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cogata <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 15:33:08 by cogata            #+#    #+#             */
-/*   Updated: 2024/02/28 15:33:13 by cogata           ###   ########.fr       */
+/*   Updated: 2024/03/04 14:30:05 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	main(void)
 	while (1)
 	{
 		prompt = readline("minishell: ");
+		if (ft_strlen(prompt))
+			add_history(prompt);
 		tokenizer(prompt);
 	}
 	return (0);
