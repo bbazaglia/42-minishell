ae75c129b60b4be28a46e7c390eef1aa9c3bc69d
Beatriz <bbazagli@student.42sp.org.br>
2024-03-05 14:34:16 -0300

    ctrl-d exits the program

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 15:33:08 by cogata            #+#    #+#             */
-/*   Updated: 2024/03/04 18:07:40 by bbazagli         ###   ########.fr       */
+/*   Updated: 2024/03/05 14:33:11 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 int	main(void)
 {
 	char	*prompt;
-	t_node *head;
+	t_node	*head;
 
 	head = NULL;
 	while (1)
 	{
 		prompt = readline("minishell: ");
+		if (!prompt)
+			exit(printf("exit\n"));
 		if (ft_strlen(prompt))
 			add_history(prompt);
 		head = tokenizer(prompt);
